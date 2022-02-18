29688e6ec7c41ba4293b4847f744e4a1033cd865
Roma <roma.ost2012@mail.ru>
2022-02-18 22:07:51 +0300

    tech/PMTS-12-refactoring-security-replenish-components

--- src/main/java/com/itransition/payment/security/service/SecurityService.java
+++ src/main/java/com/itransition/payment/security/service/SecurityService.java
@@ -1,8 +1,6 @@
 package com.itransition.payment.security.service;
 
-import com.itransition.payment.security.dto.AuthResponse;
-
 public interface SecurityService {
 
-    AuthResponse authorize();
+    String getAuthHeader();
 }

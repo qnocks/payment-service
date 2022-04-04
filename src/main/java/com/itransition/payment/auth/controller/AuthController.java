7b43959d1091db87591de67f2fb9a4e379c8c2b0
Roman Ostanin <r.ostanin@itransition.com>
2022-04-04 16:11:45 +0300

    tech/PMTS-22-clean-up-cde

--- src/main/java/com/itransition/payment/auth/controller/AuthController.java
+++ src/main/java/com/itransition/payment/auth/controller/AuthController.java
@@ -2,13 +2,11 @@ package com.itransition.payment.auth.controller;
 
 import com.itransition.payment.auth.dto.LoginRequest;
 import com.itransition.payment.auth.dto.LoginResponse;
+import com.itransition.payment.auth.dto.LogoutRequest;
 import com.itransition.payment.auth.dto.RefreshTokenRequest;
 import com.itransition.payment.auth.dto.RefreshTokenResponse;
-import com.itransition.payment.auth.dto.LogoutRequest;
 import com.itransition.payment.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;

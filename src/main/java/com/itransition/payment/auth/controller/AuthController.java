9464b11e5296f755dedd18a97c817748ca64934e
Roma <roma.ost2012@mail.ru>
2022-04-05 11:37:01 +0300

    tech/PMTS-23-update-swagger-for-auth

--- src/main/java/com/itransition/payment/auth/controller/AuthController.java
+++ src/main/java/com/itransition/payment/auth/controller/AuthController.java
@@ -6,6 +6,8 @@ import com.itransition.payment.auth.dto.LogoutRequest;
 import com.itransition.payment.auth.dto.RefreshTokenRequest;
 import com.itransition.payment.auth.dto.RefreshTokenResponse;
 import com.itransition.payment.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,20 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Authentication Controller", description = "API to manage authentication process")
 public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "Login", description = "Authenticates user with provided credentials")
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest request) {
         return authService.login(request);
     }
 
+    @Operation(summary = "Refresh token", description = "Generates new access token")
     @PostMapping("/refresh")
     public RefreshTokenResponse refreshToken(@RequestBody RefreshTokenRequest request) {
         return authService.refreshToken(request);
     }
 
+    @Operation(summary = "Logout", description = "Deleting existing session")
     @PostMapping("/logout")
     public void logout(@RequestBody LogoutRequest request) {
         authService.logout(request);
